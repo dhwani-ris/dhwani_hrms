3f89c83e5b92563680177bf81ea4ddd8d39b5fa0
pkolhetkar <prashant.kolhetkar@dhwaniris.com>
2025-01-07 13:43:30 +0000

    timesheet setting days changes

--- dhwani_hrms/public/js/timesheet.js
+++ dhwani_hrms/public/js/timesheet.js
@@ -12,7 +12,7 @@ frappe.ui.form.on("Timesheet Detail", {
     from_time: function (frm, cdt, cdn) {
 		frappe.db.get_value("Timesheet Settings", {name: 'Timesheet Settings'}, ["limit_older_timesheets"]).then((value)=>{
             if(value && value.message.limit_older_timesheets){
-					validateTime(frm, cdt, cdn, 'from_time', value.message.older_weeks, value.message.future_weeks);
+					validateTime(frm, cdt, cdn, 'from_time', value.message.older_days, value.message.future_days);
             }
         })
     },
@@ -20,27 +20,27 @@ frappe.ui.form.on("Timesheet Detail", {
     to_time: function(frm, cdt, cdn) {
 		frappe.db.get_value("Timesheet Settings", {name: 'Timesheet Settings'}, ["limit_older_timesheets"]).then((value)=>{
             if(value && value.message.limit_older_timesheets){
-				validateTime(frm, cdt, cdn, 'to_time', value.message.older_weeks, value.message.future_weeks); // Pass 'to_time' here
+				validateTime(frm, cdt, cdn, 'to_time', value.message.older_days, value.message.future_days); // Pass 'to_time' here
             }
         })
     }
 });
 
 
-function validateTime(frm, cdt, cdn, fieldname, older_weeks, future_weeks) {
+function validateTime(frm, cdt, cdn, fieldname, older_days, future_days) {
     let row = locals[cdt][cdn];
     const time = moment(row[fieldname]);
-    const past_limit = moment().subtract(older_weeks, 'weeks');
-    const future_limit = moment().add(future_weeks, 'weeks');
+    const past_limit = moment().subtract(older_days, 'days');
+    const future_limit = moment().add(future_days, 'days');
 
     if (time.isBefore(past_limit)) {
-        frappe.msgprint(__("You cannot select a " + fieldname + " older than " + older_weeks + " weeks in the past"));
+        frappe.msgprint(__("You cannot select a " + fieldname + " older than " + older_days + " weeks in the past"));
         frappe.model.set_value(cdt, cdn, fieldname, null);
         return; 
     }
 
     if (time.isAfter(future_limit)) {
-        frappe.msgprint(__("You cannot select a " + fieldname + " more than " + future_weeks + " weeks in the future."));
+        frappe.msgprint(__("You cannot select a " + fieldname + " more than " + future_days + " weeks in the future."));
         frappe.model.set_value(cdt, cdn, fieldname, null);
         return;
     }
