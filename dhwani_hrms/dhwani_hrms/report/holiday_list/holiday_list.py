144a475f702d20812fd7bc8aaf605fe300a390e5
Alok Shukla <alok.shukla@dhwaniris.com>
2025-01-30 13:30:53 +0530

    fix: Holiday List changes

--- dhwani_hrms/dhwani_hrms/report/holiday_list/holiday_list.py
+++ dhwani_hrms/dhwani_hrms/report/holiday_list/holiday_list.py
@@ -1,5 +1,3 @@
-# holiday_list.py (Script Report)
-
 import frappe
 from frappe import _
 
@@ -9,8 +7,15 @@ def execute(filters=None):
     if not filters:
         filters = {}
 
-    # Fetch holidays from the Holiday doctype
-    filters["weekly_off"] = 0
+    # Apply year filter if provided
+    if filters.get("year"):
+        filters["holiday_date"] = [
+            "between",
+            [f"{filters.get('year')}-01-01", f"{filters.get('year')}-12-31"],
+        ]
+        del filters["year"]
+
+    filters["weekly_off"] = 0  # Exclude weekly offs
     holidays = frappe.get_all(
         "Holiday",
         fields=["name", "holiday_date", "description"],
@@ -18,22 +23,24 @@ def execute(filters=None):
         order_by="holiday_date asc",
     )
 
-    # Prepare a list of dictionaries to pass to the report
     holiday_data = []
     for holiday in holidays:
+        holiday_date = frappe.utils.getdate(holiday.holiday_date)
+        day_name = holiday_date.strftime("%A")
+
         holiday_data.append(
             {
-                "name": holiday.name,
+                # "name": holiday.name,
                 "holiday_date": holiday.holiday_date,
+                "day": day_name,  # Include day name
                 "description": holiday.description,
             }
         )
 
-    # Define columns for the report (fields to display)
     columns = [
         _("Holiday Date") + ":Date:120",
+        _("Day") + ":Data:100",  # Add Day column
         _("Description") + ":Data:200",
     ]
 
-    # Return the data and columns for the report
     return columns, holiday_data
