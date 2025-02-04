008ca1cf0edc3a3447cbc7b1d0c052de02f66c91
Alok Shukla <alok.shukla@dhwaniris.com>
2025-02-04 13:25:21 +0530

    fix: Managers cannot apply comp off

--- dhwani_hrms/public/js/compensatory_leave_request.js
+++ dhwani_hrms/public/js/compensatory_leave_request.js
@@ -1,6 +1,13 @@
 frappe.ui.form.on("Compensatory Leave Request", {
     refresh(frm) {
+        customBreadCrumbs.add(frm.doctype, "Form", "Leave");
 
-        customBreadCrumbs.add(frm.doctype, "Form", "Leave")
-    },
+        if (!(frappe.user.has_role("HR Manager") || frappe.user.has_role("HR User") || frappe.user.has_role("System Manager"))) {
+            frm.set_query("employee", () => ({
+                filters: {
+                    user_id: frappe.session.user
+                }
+            }));
+        }
+    }
 });
